de136fe4db25d11f91a1b51a63a09a26d9d51835
dssff <dendssff@gmail.com>
2026-05-10 17:03:07 +0300

    stucture

---
+++ src/main/java/com/railway/MainApp.java
@@ -0,0 +1,14 @@
+package com.railway;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
